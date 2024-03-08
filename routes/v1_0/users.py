e9e8ca21595bc6fe99820ffa9871736545bcc78f
Vic <victorgt2406@github.com>
2024-03-08 13:04:24 +0100

    updates there

--- routes/v1_0/users.py
+++
@@ -1,8 +0,0 @@
-# Ejemplo de users.py en v1.0, v1.1, y beta
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/")
-async def read_users():
-    return {"message": "Lista de usuarios para versión específica en v1_0"}
